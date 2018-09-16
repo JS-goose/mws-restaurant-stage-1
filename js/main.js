815d7b63695d10515d7cdfe1f7baa79971da0800
Jonathan Sexton <joncsexton@gmail.com>
2018-09-16 15:53:35 -0500

    Accessibility: added img alt info for restaurants

--- js/main.js
+++ js/main.js
@@ -161,6 +161,7 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = `The ${restaurant.name} restaurant at ${restaurant.address}`;
   li.append(image);
 
   const name = document.createElement('h1');
