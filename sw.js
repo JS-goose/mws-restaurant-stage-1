1be4f4199d88638a2f165bbe5d19c9826cc0f773
Jonathan Sexton <joncsexton@gmail.com>
2018-09-17 08:20:11 -0500

    trying to get service worker to cache files

--- sw.js
+++ sw.js
@@ -1,10 +1,30 @@
-let staticCache = "mws-cache-v1";
-const cacheURLS = ["/index.html", "/restaurant.html", "/css/style.css"];
+const staticCache = "mws-cache-v1";
+const cacheURLS = ["index.html", "restaurant.html", "/css/style.css", "/js/main.js"];
 
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open("mws-cache-v1").then(cache => {
-      return cache.addAll(cacheURLS);
+// self.addEventListener("install", (event)  => {
+//   // event.waitUntil(
+//     caches.open("mws-cache-v1");
+//   // );
+// });
+// // 
+
+// self.addEventListener('active', (cache)=> {
+//   console.log('SW Active!');
+//   return cache.addAll(cacheURLS);
+// });
+
+self.addEventListener('install', (event) => {
+  console.log('Service Worker Installed');
+  event.waitUntil (
+    caches.open(staticCache)
+    .then((cache) => {
+      console.log("SW - Caching Files");
+      cache.addAll(cacheURLS);
     })
-  );
+    .then(() => self.skipWaiting())
+  )
+});
+
+self.addEventListener('activate', event => {
+  console.log('Service Worker Active');
 });
